557446287a45e740ac085cf37edfc03c64e15852
David Wheatley <david@davwheat.dev>
2026-04-09 22:08:53 +0100

    chore: add license headers

--- api/src/brands.ts
+++ api/src/brands.ts
@@ -1,3 +1,21 @@
+/**
+  Open Fuel Map
+  Copyright (C) 2026  David Wheatley
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // Upstream brand_name is noisy (casing variants, compound names, independent
 // garage names stored as brands). This module canonicalises each raw value
 // against a hand-curated list. Unrecognised values collapse to "Independent".
